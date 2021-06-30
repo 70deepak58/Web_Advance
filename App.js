e11f1c3bebca4ea0c04febd04bb0fc26ca9ad42f
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-06-30 21:57:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,4 @@
+<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+  
