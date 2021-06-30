648e4a4ff6d2f9b620537915429b124f975c5a32
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-06-30 22:08:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-  <script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
-  <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
-  
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+export default Car;
