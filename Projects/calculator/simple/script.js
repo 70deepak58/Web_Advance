cb7e2b3891bbec4b07dc06472e7dd9d675fdfcf1
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-10-24 10:47:15 +0530

    Add files via upload

--- Projects/calculator/simple/script.js
+++ Projects/calculator/simple/script.js
@@ -1,8 +1,3 @@
 function vibrate(){
-   // navigator.vibrate(200);
-    
-//navigator.vibrate(200); // vibrate for 200ms
-navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
-
-    
+    navigator.vibrate(1000);
 }
