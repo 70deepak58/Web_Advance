b82a5ba01de9ccef4ba58dfd1678927afd13d1c4
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-10-24 08:18:51 +0530

    Update script.js

--- Projects/calculator/simple/script.js
+++ Projects/calculator/simple/script.js
@@ -1,3 +1,8 @@
 function vibrate(){
-    navigator.vibrate(200);
+   // navigator.vibrate(200);
+    
+//navigator.vibrate(200); // vibrate for 200ms
+navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
+
+    
 }
