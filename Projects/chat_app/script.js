e33e197b490526c68afa377742569af002d88fff
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-10-28 07:52:05 +0530

    Add files via upload

--- Projects/chat_app/script.js
+++ Projects/chat_app/script.js
@@ -1,11 +1,13 @@
 let turn=0;
 let question_flag="";
 let question="";
+let turn_audio=0;
 let dict_reply={"a":"apple","b":"boy","c":"cat","d":"dog","e":"elephant","f":"frog","g":"goat","h":"hen","i":"ice-cream","j":"jackpot","k":"key",
                  "l":"lion","m":"mango","n":"nose","o":"onion","P":"printer","q":"queen","r":"rose","s":"sea","t":"toy","u":"uniform","v":"van",
                  "w":"watch","x":"x-ray","y":"yak","z":"zoo"
 };              
 var l="";
+let final_transcript = "";
 //system turn
 function replydo(){
     const par=document.createElement("p");
@@ -16,6 +18,7 @@ function replydo(){
     ele.appendChild(par);
     text_to_speech_for_system();
     turn++;
+    turn_audio++;
     l="";
 
 }
@@ -57,5 +60,33 @@ speech.volume=1;
 speech.rate=1;
 speech.pitch=1;
 window.speechSynthesis.speak(speech);
+
+}
+
+function text_to_speech_for_user(){
+    let speechRecognition = new webkitSpeechRecognition();
+    
+  
+    //speechRecognition.continuous = true;
+    speechRecognition.interimResults = true;
+    speechRecognition.lang="en-US";
+
+
+    speechRecognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        //var confidence = event.results[0][0].confidence;
+        transcript=transcript.toLowerCase();
+        l=transcript[0];
+        replydo();
+        console.log(transcript);
+    };
+
+    document.querySelector("#btn2").onclick = () => {
+        speechRecognition.start();
+      };
+      document.querySelector("#btn3").onclick = () => {
+        speechRecognition.stop();
+      };
+      final_transcript="";
 
 }
