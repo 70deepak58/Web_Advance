ed31104348fbe39da7145a05b6541b7db226eb1b
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-10-28 07:20:59 +0530

    Add files via upload

--- Projects/chat_app/script.js
+++ Projects/chat_app/script.js
@@ -6,6 +6,7 @@ let dict_reply={"a":"apple","b":"boy","c":"cat","d":"dog","e":"elephant","f":"fr
                  "w":"watch","x":"x-ray","y":"yak","z":"zoo"
 };              
 var l="";
+//system turn
 function replydo(){
     const par=document.createElement("p");
     par.textContent=dict_reply[l];
@@ -13,11 +14,12 @@ function replydo(){
     //par.appendChild(go);
     const ele=document.getElementById("div1");
     ele.appendChild(par);
+    text_to_speech_for_system();
     turn++;
     l="";
 
 }
-
+//user turn
 function create(){
     var x=document.getElementById("ipt");
     question=x.value;
@@ -45,4 +47,15 @@ function create(){
 function maketext(){
     var x=document.getElementById("ipt");
     question=x.value;
+}
+
+function text_to_speech_for_system(){
+let speech=new SpeechSynthesisUtterance();
+speech.lang="en-US";
+speech.text=dict_reply[l];
+speech.volume=1;
+speech.rate=1;
+speech.pitch=1;
+window.speechSynthesis.speak(speech);
+
 }
