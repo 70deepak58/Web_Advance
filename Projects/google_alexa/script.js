76170121a154100d04de74760c2a9be6256806ca
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2021-10-29 07:40:42 +0530

    Add files via upload

---
+++ Projects/google_alexa/script.js
@@ -0,0 +1,47 @@
+let speechRecognition = new webkitSpeechRecognition();
+let final_transcript = "";
+let interim_result="";
+speechRecognition.continuous = true;
+speechRecognition.interimResults = true;
+speechRecognition.lang = "en-US";
+
+speechRecognition.onspeechend = function () {
+    //speechRecognition.stop();
+    console.log("stop");
+}
+speechRecognition.onstart = function () {
+    question_audio = "";
+    console.log("start");
+}
+speechRecognition.onend = () => {
+    console.log("end");
+};
+speechRecognition.onresult = (event) => {
+
+    interim_transcript = "";
+    //final_transcript = "";
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            final_transcript= event.results[i][0].transcript;
+        } else {
+            interim_transcript += event.results[i][0].transcript;
+        }
+    }
+
+    //uncomment
+    //final_transcript= event.results[0][0].transcript;
+    //final_transcript= event.results[1][0].transcript;
+   // question_audio = final_transcript.toLowerCase();
+    console.log(final_transcript);
+    document.getElementById("container").innerHTML=final_transcript;
+};
+
+
+
+document.querySelector("#start").onclick = () => {
+    speechRecognition.start();
+  };
+  document.querySelector("#stop").onclick = () => {
+    speechRecognition.stop();
+  };
+
