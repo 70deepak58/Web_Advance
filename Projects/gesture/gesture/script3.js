1d303cf97b5fa083c2f9a72201c67068b8b43ce7
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2022-05-17 19:33:15 +0530

    Add files via upload

---
+++ Projects/gesture/gesture/script3.js
@@ -0,0 +1,207 @@
+console.log("testing");
+var loca="";
+var music_flag=1;
+var image_flag=1;
+var x;
+var rf1=1,rf2=1,rf3=1,rf4=1,lf1=1,lf2=1,lf3=1,lf4=1;
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  // if (results.multiHandLandmarks) {
+  //   for (const landmarks of results.multiHandLandmarks) {
+  //     drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+  //                    {color: '#00FF00', lineWidth: 5});
+  //     drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+  //   }
+  // }
+  if (results.multiHandLandmarks && results.multiHandedness) {
+for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+  const classification = results.multiHandedness[index];
+  const isRightHand = classification.label === 'Right';
+  const landmarks = results.multiHandLandmarks[index];
+
+  //connector and dot 
+  drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+      {color: isRightHand ? '#00FF00' : '#FF0000'});
+  drawLandmarks(canvasCtx, landmarks, {
+    color: isRightHand ? '#00FF00' : '#FF0000',
+    fillColor: isRightHand ? '#FF0000' : '#00FF00',
+    radius: (x) => {
+      return lerp(x.from.z, -0.15, .1, 10, 1);
+    }       
+  });
+  // if(isRightHand == true){
+
+  // }
+  // else{
+
+  // }
+
+
+  //all logics here
+  const hand = results.multiHandLandmarks[0]; 
+  if(isRightHand == true){
+
+    //start right
+    if((hand[8].y<hand[6].y) && rf1==1){
+      rf1=0;
+    console.log("down");
+    document.getElementById("tak").style.backgroundColor="rgb("+Math.floor(Math.random() * 256)+","+Math.floor(Math.random() * 256)+","+Math.floor(Math.random() * 256)+")";
+  }
+  if((hand[20].y>hand[18].y) && rf2==1){
+    rf2=0;
+    console.log("vibrate");
+    // document.getElementById("tak").style.backgroundColor="red";
+    navigator.vibrate(1000);
+  }
+  if((hand[12].y>hand[10].y) && rf3==1){
+    rf3=0;
+    // console.log("down");
+    // document.getElementById("tak").style.backgroundColor="red";
+    loca=document.getElementById("location");
+    getLocation();
+    console.log("location");
+  }
+
+  if((hand[16].y<hand[14].y) && rf4==1){
+    rf4=0;
+    image_flag++;
+    if(image_flag>=18){
+        image_flag=1;
+    }
+  x=document.getElementById("image");
+  x.setAttribute("src","image/"+image_flag+".jpeg");
+ // x.load();
+  console.log("next image");
+  }
+  //end right
+      if((hand[8].y>hand[6].y)){
+      rf1=1;}
+      if((hand[20].y>hand[18].y)){
+      rf2=1;}
+      if((hand[12].y>hand[10].y)){
+      rf3=1;}
+      if((hand[16].y>hand[14].y)){
+      rf4=1;}
+
+  }
+  else{
+    //other hand
+    //middle finger
+    if((hand[12].y<hand[10].y) && lf1==1){
+      lf1=0;
+      x=document.getElementById("music");
+    x.play();
+    console.log("music");
+  }
+  //ring finger
+  if((hand[16].y<hand[14].y) && lf2==1){
+    lf2=0;
+    x=document.getElementById("music");
+    x.pause();
+    console.log("music stop");
+  }
+  //index finger
+  if((hand[8].y<hand[6].y) && lf3==1){
+    lf3=0;
+    music_flag++;
+    if(music_flag>=4){
+        music_flag=1;
+    }
+    x=document.getElementById("music");
+   var sour=document.getElementById("sr");
+   sour.setAttribute("src",music_flag+".mp3");
+   x.load();
+   x.play();
+   console.log("music next");
+  }
+  //pinky finger
+  if((hand[20].y<hand[18].y) && lf4==1){
+    lf4=0;
+    music_flag--;
+    if(music_flag<=-0){
+        music_flag=3;
+    }
+    x=document.getElementById("music");
+   var sour=document.getElementById("sr");
+   sour.setAttribute("src",music_flag+".mp3");
+   x.load();
+   x.play();
+   console.log("music previous");
+  }
+
+
+  //end of other hand
+      if((hand[8].y>hand[6].y)){
+      lf1=1;}
+      if((hand[20].y>hand[18].y)){
+      lf2=1;}
+      if((hand[12].y>hand[10].y)){
+      lf3=1;}
+      if((hand[16].y>hand[14].y)){
+      lf4=1;}
+  }
+
+
+
+  //dhfjhd edy ruyehgfewhjh fydjhfhnd gfjedyh fjkdh
+
+}
+}
+
+
+
+
+  ////
+  canvasCtx.restore();
+}
+
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
+
+
+
+
+
+
+
+
+//location
+//const x = document.getElementById("demo");
+
+function getLocation() {
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(showPosition);
+} else { 
+loca.innerHTML = "Geolocation is not supported by this browser.";
+}
+}
+
+function showPosition(position) {
+loca.innerHTML = "Latitude: " + position.coords.latitude + 
+"<br>Longitude: " + position.coords.longitude;
+}
+//end location
