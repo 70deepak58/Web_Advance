7d7f995028c64439ffacc5d88cc1137fb6dde5a9
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2023-03-31 10:47:22 +0530

    Add files via upload

---
+++ my_grav/script.js
@@ -0,0 +1,38 @@
+console.log("hello ssgdhsgdh");
+var x=560,y=300, d=0;;
+
+let sensor = new GravitySensor({ frequency: 60 });
+sensor.start();
+
+sensor.onreading = () => {
+	document.getElementById("p1").innerHTML=sensor.x;
+	document.getElementById("p2").innerHTML=sensor.y;
+	document.getElementById("p3").innerHTML=sensor.z;
+    console.log("Angular velocity around the X-axis " + sensor.x);
+    console.log("Angular velocity around the Y-axis " + sensor.y);
+    console.log("Angular velocity around the Z-axis " + sensor.z);
+	if(sensor.x>0){
+		d=d+20;
+	}
+	else{
+		d=d-20;
+	}
+	document.getElementById("target").style.top=y+d+"px";
+document.getElementById("p4").innerHTML=y+d;
+};
+
+sensor.onerror = event => console.log(event.error.name, event.error.message);
+
+
+
+/*
+let gravitySensor = new GravitySensor({ frequency: 60 });
+
+gravitySensor.addEventListener("reading", (e) => {
+  console.log(`Gravity along the X-axis ${gravitySensor.x}`);
+  console.log(`Gravity along the Y-axis ${gravitySensor.y}`);
+  console.log(`Gravity along the Z-axis ${gravitySensor.z}`);
+});
+
+gravitySensor.start();
+*/
