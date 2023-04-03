a75196a6bc0c594113a20b415896e8fbdda3c523
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2023-04-03 10:07:36 +0530

    Update script.js

--- my_grav_game/script.js
+++ my_grav_game/script.js
@@ -1,6 +1,6 @@
 console.log("hello ssgdhsgdh");
 var gx=560,gy=300, cy=0 ,cx=0, pts=0,tx=860,ty=400;
-
+/*
 tar_update();
 function tar_update(){
     tx=60*Math.floor(10*Math.random());
@@ -11,7 +11,7 @@ function tar_update(){
 }
 
 
-
+*/
 let sensor = new GravitySensor({ frequency: 60 });
 sensor.start();
 
@@ -25,31 +25,37 @@ sensor.onreading = () => {
 	//for y
 	if(sensor.y> 0.5){
 		cy=cy+5;
+		gy=gy+5;
 	}
 	else if(sensor.y< -0.5){
 		cy=cy-5;
+		gy=gy-5;
 	}
-	document.getElementById("gun").style.top=gy+cy+"px";
+	//document.getElementById("gun").style.top=gy+cy+"px";
+	document.getElementById("gun").style.top=gy+"px";
     document.getElementById("p4").innerHTML=gy+cy;
 	
 	//for x
 	if(sensor.x> 0.5){
-		cx=cx-5;		
+		cx=cx-5;
+        gx=gx-5;		
 	}
 	else if(sensor.x< -0.5){
 		cx=cx+5;
+		gx=gx+5;
 	}
-	document.getElementById("gun").style.left=gx+cx+"px";
+	//document.getElementById("gun").style.left=gx+cx+"px";
+	document.getElementById("gun").style.left=gx+"px";
 	
-	if(Math.abs(tx-gx-cx)<10  && Math.abs(ty-gy-cy)<10){
+	if(Math.abs(tx-gx)<10  && Math.abs(ty-gy)<10){
 		pts=pts+1;
 		document.getElementById("pts").innerHTML=pts;
 		tx=60*Math.floor(10*Math.random());
-                ty=35*Math.floor(10*Math.random());
+        ty=35*Math.floor(10*Math.random());
 		document.getElementById("tgt").style.top=ty+"px";
 		document.getElementById("tgt").style.left=tx+"px";
 	}
-	document.getElementById("dummy").innerHTML=tx+"a"+gx+cx+"a"+ty+"a"+gy+cy;
+	document.getElementById("dummy").innerHTML=tx+"a"+gx+"mam"+ty+"a"+gy;
 	/*
 	if(Math.abs(tx-gx-cx)<10  && Math.abs(ty-gy-cy)<10){
 		pts=pts+1;
