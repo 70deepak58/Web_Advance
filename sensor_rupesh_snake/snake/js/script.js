28ba4f16c59c0b6ae186cccea1cce1ef57bde50c
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2023-10-10 09:01:42 +0530

    Update script.js

--- sensor_rupesh_snake/snake/js/script.js
+++ sensor_rupesh_snake/snake/js/script.js
@@ -154,37 +154,36 @@ window.addEventListener('keydown', e =>{
 function abc(){
 
 
-let sensor = new GravitySensor({ frequency: 60 });
-
-sensor.start();
-sensor.onreading = () => {
-
-
-	//for x
-    inputDir = {x: 0, y: 1} // Start the game
-    moveSound.play();
-	if(sensor.x< -0.5){
-        console.log("ArrowLeft");
-        inputDir.x = -1;
-        inputDir.y = 0;
-		
-	}
-	else if(sensor.x> 0.5){
-        console.log("ArrowRight");
-        inputDir.x = 1;
-        inputDir.y = 0;
-	}
-    else if(sensor.y> 0.5){
-        console.log("ArrowDown");
-        inputDir.x = 0;
-        inputDir.y = 1;
-
-    }else{
-        console.log("ArrowUp");
-        inputDir.x = 0;
-        inputDir.y = -1;
+    let sensor = new GravitySensor({ frequency: 60 });
+    
+    sensor.start();
+    sensor.onreading = () => {
+    
+    
+        //for x
+        inputDir = {x: 0, y: 1} // Start the game
+        moveSound.play();
+        if(sensor.x< -0.9){
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+        }
+        else if(sensor.x> 0.9){
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+        }
+        else if(sensor.y> 0.9){
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+    
+        }else if(sensor.y< -0.9){
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+        }
+    };
+    
+    sensor.onerror = event => console.log(event.error.name, event.error.message);
     }
-};
-
-sensor.onerror = event => console.log(event.error.name, event.error.message);
-}
