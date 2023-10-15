3b56fc649102a8bf06516310b18d0a0b09c216a2
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2023-10-15 17:40:59 +0530

    Add files via upload

---
+++ Projects/ChromeExt/chrome_extension_web_gesture_control/popup.js
@@ -0,0 +1,36 @@
+const zu = document.getElementById("zu");
+const zd = document.getElementById("zd");
+const dinst = document.getElementById("dinst");
+var zoom_flag=1;
+var text="ram";
+
+
+zu.addEventListener("click", () => {
+  zoom_flag=zoom_flag+0.1;
+  chrome.tabs.executeScript({
+    code: `
+    document.body.style.zoom=${zoom_flag};
+    `
+  });
+});
+zd.addEventListener("click", () => {
+  zoom_flag=zoom_flag-0.1;
+  chrome.tabs.executeScript({
+    code: `
+    document.body.style.zoom=${zoom_flag};
+    `
+  });
+});
+dinst.addEventListener("click", () => {
+  chrome.tabs.executeScript({
+    code: `
+    var divElement = document.createElement("div");
+    divElement.id = "videoContainer";
+    document.body.appendChild(divElement);
+    loadScript('https://cdn.jsdelivr.net/gh/70deepak58/Web_Advance/popup.js', function() {
+      cnt++;
+      console.log('Script 49 has been loaded.');
+    });
+    `
+  });
+});
